83b534e9785161532f69b33165f54deb48e564fd
shanqyeet <shanqyeet@gmail.com>
2019-04-11 14:46:38 +0800

    installed jest

---
+++ script.js
@@ -0,0 +1,16 @@
+const googleDatabase = [
+    'cats.com',
+    'souprecipes.com',
+    'trustmaven.co',
+    'animals.com',
+    'funnyprogrammers.com',
+    'github.com'
+];
+
+const googleSearch = (searchInput) => {
+    const matches = googleDatabase.filter(website => {
+        return website.includes(searchInput);
+    })
+
+    return matches.length > 3 ? matches.slice(0,3) : matches
+}
